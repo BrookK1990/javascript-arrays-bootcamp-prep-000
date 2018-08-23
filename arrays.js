55fa51ad50a19ac6f0d353e84cbc6603362b014e
bkalk08@yahoo.com <brook.kalkhoven@gmail.com>
2018-08-23 17:22:20 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(array,element){
+  var array = [1]
+  var element =  "foo"
+return [element,...array]
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  var array = [1]
+  var element = "foo"
+  return array.push(element)
+}
